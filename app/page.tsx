929d072d05265d667c301bb05adcbea96fc00641
allanlalangan <allanlalangan@gmail.com>
2023-06-07 23:21:28 -0700

    ???

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,29 @@
-import TodayChart from "@/components/TodayChart";
 import Link from "next/link";
-
 import { getNow } from "@/lib/astro";
-import Hero from "@/components/Hero";
+import TodayChart from "@/components/TodayChart";
 
 export default async function Home() {
   const now = await getNow();
   return (
     <main className="flex min-h-screen flex-col justify-center p-4">
-      <Hero />
+      <section className="flex flex-col justify-center">
+        <h1 className="text-6xl uppercase tracking-wide font-display text-accent-400">
+          Stars
+        </h1>
+        <p className="mb-2 text-2xl font-serif text-secondary-500">
+          the astrology app
+        </p>
+        <p className="mb-2">
+          Create personalized natal charts and view upcoming astrological
+          events. Tap in to your guides with <em>Stars</em>.
+        </p>
+        <Link
+          className="bg-primary-300 w-fit px-4 py-2 mt-2 hover:text-accent-300 transition hover:bg-primary-400 active:bg-primary-500"
+          href="/sign-in"
+        >
+          Sign In
+        </Link>
+      </section>
       <TodayChart now={now.data} />
     </main>
   );
