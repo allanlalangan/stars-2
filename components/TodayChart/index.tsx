ef62b226006dc20806c23d3432d4ded42241699e
allanlalangan <allanlalangan@gmail.com>
2023-06-07 23:23:27 -0700

    unseparate lib

--- components/TodayChart/index.tsx
+++ components/TodayChart/index.tsx
@@ -1,5 +1,36 @@
 import { getNow } from "@/lib/astro";
-import { getFullSignName } from "@/lib/util";
+
+const getFullSignName = (id: number) => {
+  switch (id) {
+    case 0:
+      return "Aries";
+    case 1:
+      return "Taurus";
+    case 2:
+      return "Gemini";
+    case 3:
+      return "Cancer";
+    case 4:
+      return "Leo";
+    case 5:
+      return "Virgo";
+    case 6:
+      return "Libra";
+    case 7:
+      return "Scorpio";
+    case 8:
+      return "Sagittarius";
+    case 9:
+      return "Capricorn";
+    case 10:
+      return "Aquarius";
+    case 11:
+      return "Pisces";
+    default:
+      return "Unknown";
+  }
+};
+
 type props = {
   now: any;
 };
