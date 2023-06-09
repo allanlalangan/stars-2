48dc21a23e240f02d97a82fe5edc90744a927010
allanlalangan <allanlalangan@gmail.com>
2023-06-08 21:08:09 -0700

    add fetch, no render

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { getNow } from "@/lib/astro";
 import Link from "next/link";
 
 export default async function Home() {
-  // const now = await getNow();
+  const now = await getNow();
   return (
     <main className="flex min-h-screen flex-col justify-center p-4">
       <Hero />
