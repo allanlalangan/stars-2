dbf6b676764715ce064cb67b463bf2dcb82d7b28
allanlalangan <allanlalangan@gmail.com>
2023-06-09 21:41:08 -0700

    update color palette, styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import Hero from "@/components/Hero";
 import TodayChart from "@/components/TodayChart";
-import { getNow } from "@/lib/astro";
-import Link from "next/link";
 
 export default async function Home() {
-  const now = await getNow();
   return (
     <main className="flex min-h-screen flex-col justify-center p-4">
       <Hero />
-      <TodayChart data={now} />
+      <TodayChart />
     </main>
   );
 }
