32f8cdb8fa031c356dc9e313f35aa6a0056b04f4
allanlalangan <allanlalangan@gmail.com>
2023-06-24 12:26:25 -0700

    update table, glyphs and retrograde symbols

--- lib/util.ts
+++ lib/util.ts
@@ -28,3 +28,59 @@ export function getFullSignName(id?: number) {
       return "Unknown";
   }
 }
+
+export function getSignGlyph(id?: number) {
+  switch (id) {
+    case 0:
+      return "♈︎";
+    case 1:
+      return "♉︎";
+    case 2:
+      return "♊︎";
+    case 3:
+      return "♋︎";
+    case 4:
+      return "♌︎";
+    case 5:
+      return "♍︎";
+    case 6:
+      return "♎︎";
+    case 7:
+      return "♏︎";
+    case 8:
+      return "♐︎";
+    case 9:
+      return "♑︎";
+    case 10:
+      return "♒︎";
+    case 11:
+      return "♓︎";
+    default:
+      return "Unknown";
+  }
+}
+
+export function getPlanetGlyph(planet?: string) {
+  switch (planet) {
+    case "Sun":
+      return "☉";
+    case "Moon":
+      return "☽";
+    case "Mercury":
+      return "☿";
+    case "Venus":
+      return "♀︎";
+    case "Mars":
+      return "♂︎";
+    case "Jupiter":
+      return "♃";
+    case "Saturn":
+      return "♄";
+    case "Uranus":
+      return "♅";
+    case "Neptune":
+      return "♆";
+    case "Pluto":
+      return "♇";
+  }
+}
